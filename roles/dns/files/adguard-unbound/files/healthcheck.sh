596c309f59ee98d8c518e53ec69c9f441c09151c
kw <115936263+k-wlosek@users.noreply.github.com>
2023-07-24 19:35:12 +0200

    fix(adguard): Use path to cert, restart if invalid

--- roles/dns/files/adguard-unbound/files/healthcheck.sh
+++ roles/dns/files/adguard-unbound/files/healthcheck.sh
@@ -9,3 +9,8 @@ elif [PORT="$(cat $FILE | grep '^bind_port:' | cut -f2 -d' ')" && ! curl -q -t 1
 then
     exit 1
 fi
+
+if ! openssl x509 -checkend 0 -noout -in /opt/adguardhome/conf/fullchain.pem
+then
+  kill 1
+fi
